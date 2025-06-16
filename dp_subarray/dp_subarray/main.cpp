7f303c00d3ae01c9d48e5acc2ee613e3f274da7a
Parihar Naresh Singh <pariharsoftwdev@gmail.com>
2025-06-17 03:43:37 +0530

    updates

--- dp_subarray/dp_subarray/main.cpp
+++ dp_subarray/dp_subarray/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
+
+void printSubarrayHelper(vector<int> &A, int idx)
+{
+    if(idx>A.size()) return;
+    for(int i=idx;i<A.size();i++){
+        cout<<A[i]<<" ";
+    }
+    cout<<"\n";
+    printSubarrayHelper(A, idx+1);
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    vector<int> A{1,2,3};
+    for(int i=0;i<A.size();i++){
+        printSubarrayHelper(A, i);
+        cout<<endl;
+    }
+    std::cout << "Hello, understanding subsequesntce or subset or powerset!\n";
     return 0;
 }
--- sync_locks_mutex_parking/sync_locks_mutex_parking/main.cpp
+++ sync_locks_mutex_parking/sync_locks_mutex_parking/main.cpp
@@ -6,9 +6,57 @@
 //
 
 #include <iostream>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+using namespace std;
+
+class Parking{
+private:
+    mutex mtx;
+    condition_variable cv;
+    int counter;
+public:
+    Parking(int count):counter(count){};
+    void acquire(){
+        unique_lock<mutex> acq_uni_lock(mtx);
+        while(counter == 0)
+        {
+            cout<<"Parking slots full,  waiting to be freed..!: "<<counter<<endl;
+            cv.wait(acq_uni_lock);
+        }
+        counter--;
+    }
+    
+    void release(){
+        unique_lock<mutex> rel_uni_lock(mtx);
+        counter--;
+        cv.notify_one();
+    }
+};
+
+Parking parking_slots(3);
+
+void carParkingWorker(int carId){
+    cout<<"car parking : "<<carId<<endl;
+    parking_slots.acquire();
+    cout<<"car parked : "<<carId<<endl;
+    this_thread::sleep_for(chrono::seconds(3));
+    cout<<"cark leaving : "<<carId<<endl;
+    parking_slots.release();
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, Understanding locks mutex etx!\n";
+    
+    thread cars[5];
+    for(int i =0;i<5;i++)
+    {
+        cars[i] = thread(carParkingWorker, 1000+i);
+    }
+    for(int i=0;i<5;i++){
+        cars[i].join();
+    }
     return 0;
 }
